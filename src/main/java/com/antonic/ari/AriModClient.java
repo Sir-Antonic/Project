f5f79896b02f6ecd623dc1163b2354a7724a2a93
Sir-Antonic <sirlovelady@gmail.com>
2023-03-28 16:28:13 -0500

    Initial Commit

---
+++ src/main/java/com/antonic/ari/AriModClient.java
@@ -0,0 +1,12 @@
+package com.antonic.ari;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AriModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient(){
+      // TODO document why this method is empty
+    }
+    
+}
---
+++ src/main/java/com/antonic/ari/mixin/AriMixin.java
@@ -0,0 +1,16 @@
+package com.antonic.ari.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.antonic.ari.AriMod;
+
+@Mixin(TitleScreen.class)
+public class AriMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		AriMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
