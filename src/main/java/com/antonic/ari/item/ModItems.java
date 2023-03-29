dd19deef524ce3643bc874fb9c4a6fdea655401c
Sir-Antonic <sirlovelady@gmail.com>
2023-03-28 19:44:36 -0500

    Sticks Made

--- src/main/java/com/antonic/ari/item/ModItems.java
+++ src/main/java/com/antonic/ari/item/ModItems.java
@@ -14,6 +14,15 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item OAKSTICK = registerItem("oakstick", new Item(new FabricItemSettings()));
+    public static final Item SPRUCESTICK = registerItem("sprucestick", new Item(new FabricItemSettings()));
+    public static final Item BIRCHSTICK = registerItem("birchstick", new Item(new FabricItemSettings()));
+    public static final Item JUNGLESTICK = registerItem("junglestick", new Item(new FabricItemSettings()));
+    public static final Item ACACIASTICK = registerItem("acaciastick", new Item(new FabricItemSettings()));
+    public static final Item DARKOAKSTICK = registerItem("darkoakstick", new Item(new FabricItemSettings()));
+    public static final Item MANGROVESTICK = registerItem("mangrovestick", new Item(new FabricItemSettings()));
+    public static final Item CHERRYSTICK = registerItem("cherrystick", new Item(new FabricItemSettings()));
+    public static final Item CRIMSONSTICK = registerItem("crimsonstick", new Item(new FabricItemSettings()));
+    public static final Item WARPEDSTICK = registerItem("warpedstick", new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(AriMod.MOD_ID, name), item);
@@ -21,6 +30,15 @@ public class ModItems {
 
     public static void addItemsToItemGroup(){
         addItemsToItemGroup(ItemGroups.INGREDIENTS, OAKSTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, SPRUCESTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, BIRCHSTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, JUNGLESTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, ACACIASTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, DARKOAKSTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, MANGROVESTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, CHERRYSTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, CRIMSONSTICK);
+        addItemsToItemGroup(ItemGroups.INGREDIENTS, WARPEDSTICK);
     }
 
     public static void addItemsToItemGroup(ItemGroup group, Item item) {
